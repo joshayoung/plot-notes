817472ed2d5b82f6e9b33f8ee9a00d34d352341b
Josh Young <18503267+joshayoung@users.noreply.github.com>
2018-07-03 17:35:05 -0500

    Add failing model tests

---
+++ spec/models/list_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe List, type: :model do
+  context "validation tests"
+  it "list creation requires a title" do
+    list = build(:list, title: nil).save
+    expect(list).to eq(false)
+  end
+  it "list saves with a valid title" do
+    list = build(:list).save
+    expect(list).to eq(true)
+  end
+end
