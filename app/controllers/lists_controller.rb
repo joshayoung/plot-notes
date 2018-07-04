c3792b0d9d09c003aedf3a0ea65208396a614501
Josh Young <j22cal@gmail.com>
2018-07-04 11:27:43 -0500

    Add code so feature tests will pass

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,15 +1,40 @@
 class ListsController < ApplicationController
   def index; end
 
-  def show; end
+  def show
+    @list = List.find_by(id: params[:id])
+  end
 
-  def new; end
+  def new
+    @list = List.new
+  end
 
-  def edit; end
+  def edit
+    @list = List.find_by(id: params[:id])
+  end
 
-  def create; end
+  def create
+    @list = List.new(title: params[:list][:title])
+    if @list.save
+      flash[:notice] = "List Saved Successfully"
+      redirect_to lists_url
+    else
+      render :new
+    end
+  end
 
-  def update; end
+  def update
+    @list = List.find_by(id: params[:id])
+    if @list.update(title: params[:list][:title])
+      flash[:notice] = "List Updated Successfully!"
+      redirect_to(lists_url)
+    else
+      render :edit
+    end
+  end
 
-  def destroy; end
+  def destroy
+    List.find(params[:id]).destroy
+    redirect_to(lists_url)
+  end
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,3 @@
 class List < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: { message: "Title is Required!" }
 end
