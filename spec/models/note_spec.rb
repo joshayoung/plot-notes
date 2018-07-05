617baea2ce5a1bf017e0bc3b3fa833fd1dd89af6
Josh Young <18503267+joshayoung@users.noreply.github.com>
2018-07-05 18:31:04 -0500

    Write failing model tests for note

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,5 +1,22 @@
 require "rails_helper"
 
 RSpec.describe Note, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validation with a list" do
+    it "notes require a title" do
+      list = build(:list).save
+      note = build(:note, title: nil, list_id: list).save
+      expect(note).to eq(false)
+    end
+    it "saves with a title" do
+      list = build(:list).save
+      note = build(:note, list_id: list).save
+      expect(note).to eq(true)
+    end
+  end
+  context "validation without a list" do
+    it "does not save" do
+      note = build(:note).save
+      expect(note).to eq(false)
+    end
+  end
 end
