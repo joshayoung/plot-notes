08ddd57a768e7cebc416b10aeff49bdf74909460
Josh Young <j22cal@gmail.com>
2018-07-05 18:31:04 -0500

    Write failing model tests for note

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,13 +1,14 @@
 require "rails_helper"
 
 RSpec.describe List, type: :model do
-  context "validation tests"
-  it "list creation requires a title" do
-    list = build(:list, title: nil).save
-    expect(list).to eq(false)
-  end
-  it "list saves with a valid title" do
-    list = build(:list).save
-    expect(list).to eq(true)
+  context "validation tests" do
+    it "list creation requires a title" do
+      list = build(:list, title: nil).save
+      expect(list).to eq(false)
+    end
+    it "list saves with a valid title" do
+      list = build(:list).save
+      expect(list).to eq(true)
+    end
   end
 end
