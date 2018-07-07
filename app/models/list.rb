2dba7134bcecfc2f5217a2c49b242041cb4b81a2
Josh Young <18503267+joshayoung@users.noreply.github.com>
2018-07-07 12:03:05 -0500

    Correct failing note feature tests

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,4 @@
 class List < ApplicationRecord
   validates :title, presence: { message: "Title is Required!" }
+  has_many :notes, dependent: :destroy
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,4 @@
 class Note < ApplicationRecord
   belongs_to :list
-  validates :title, presence: true
+  validates :title, presence: { message: "Title is Required!" }
 end
