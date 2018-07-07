c4d414e4f713677e88aeaa63d62821864d3ffaf0
Josh Young <j22cal@gmail.com>
2018-07-07 12:03:05 -0500

    Correct failing note feature tests

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,7 +1,41 @@
 class NotesController < ApplicationController
-  def create; end
 
-  def update; end
+  def show
+    list = List.find_by(id: params[:list_id])
+    @note = list.notes.find_by(id: params[:id])
+  end
+
+  def new
+    @list = List.find_by(id: params[:list_id])
+    @note = Note.new;
+  end
+
+  def edit
+    @list = List.find_by(id: params[:list_id])
+    @note = @list.notes.find_by(id: params[:id])
+  end
+
+  def create
+    list = List.find_by(id: params[:list_id])
+    @note = list.notes.new(title: params[:note][:title], details: params[:note][:details])
+    if @note.save
+      flash[:notice] = "Note Saved Successfully"
+      redirect_to list_notes_path(list)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @list = List.find_by(id: params[:list_id])
+    @note = @list.notes.find_by(id: params[:id])
+    if (@note.update(title: params[:note][:title], details: params[:note][:details]))
+      flash[:notice] = "Note Updated Successfully!"
+      redirect_to(list_notes_path(@list))
+    else
+      render :edit
+    end
+  end
 
   def destroy; end
 end
