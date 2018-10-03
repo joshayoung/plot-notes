75a0eaf7fcf4761806efc6e8e1fd5448b500d23d
Josh Young <j22cal@gmail.com>
2018-10-02 19:09:54 -0500

    Add route and files for archived and completed lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root "lists#index"
   resources :lists do
+    member do
+      get "archived"
+      get "completed"
+    end
     resources :notes do
       post :archive, on: :member
       post :complete, on: :member
