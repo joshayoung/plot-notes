1640f9cf46c179ce8cdf7d45683e91eb910ef89d
Josh Young <18503267+joshayoung@users.noreply.github.com>
2018-10-04 18:03:33 -0500

    Cleanup models

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,6 @@
 class List < ApplicationRecord
-  validates :title, presence: { message: "Title is Required!" }, length: { maximum: 20 }
   has_many :notes, dependent: :destroy
+
+  validates :title, presence: { message: "Title is Required!" },
+                    length: { maximum: 20 }
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,20 +1,11 @@
 class Note < ApplicationRecord
   belongs_to :list
-  validates :title, presence: { message: "Title is Required!" }, length: { maximum: 20 }
+
+  validates :title, presence: { message: "Title is Required!" },
+                    length: { maximum: 20 }
   validates :details, length: { maximum: 150 }
 
-  class << self
-    def archived
-      where("archived = '1'")
-    end
-
-    def completed
-      where("completed = '1'")
-    end
-
-    def active
-      where("completed != '1' AND archived != '1'")
-    end
-  end
-
+  scope :archived, -> { where(archived: 1) }
+  scope :completed, -> { where(completed: 1) }
+  scope :active, -> { where("archived != 1 AND completed != 1") }
 end
