59dfdd9ff61736d84a6d00fb8be85150546a77c6
Josh Young <18503267+joshayoung@users.noreply.github.com>
2019-07-13 11:48:22 -0500

    Add submit functionality to the tags form

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,4 +4,11 @@ class TagsController < ApplicationController
     @note = Note.find_by(id: params[:note_id])
     @tag = Tag.new
   end
+
+  def create
+    @list = List.find(params[:list_id])
+    @note = Note.find(params[:note_id])
+    flash[:notice] = "You have successfully added a new tag"
+    redirect_to list_note_url(@list, @note)
+  end
 end
