3df76c41f0b393a00a044f96fa93a26ca680d230
Josh Young <j22cal@gmail.com>
2019-07-14 16:25:08 -0500

    Add search form and get/post routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   root "homes#index"
   get "archived-notes", to: "homes#archived_notes"
   get "completed-notes", to: "homes#completed_notes"
+
   get "search", to: "homes#search"
+  get "results", to: "homes#results"
+  post "search", to: "homes#search_results"
+
   resources :lists do
     member do
       get "archived"
